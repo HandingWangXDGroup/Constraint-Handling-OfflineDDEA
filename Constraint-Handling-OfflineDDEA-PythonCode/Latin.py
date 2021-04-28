b0a241824a93743ff5934002a7fce30e75895fc0
Pengfei Huang <pfeihuang@foxmail.com>
2021-04-28 14:06:58 +0800

    Update Latin.py

--- Constraint-Handling-OfflineDDEA-PythonCode/Latin.py
+++ Constraint-Handling-OfflineDDEA-PythonCode/Latin.py
@@ -15,28 +15,19 @@
 
 import numpy as np
 
-def latin(N ,D,lb,ub):
-    d = 1.0/N
+def latin(N, D, lower_bound, upper_bound):
+    d = 1.0 / N
     result = np.empty([N, D])
     temp = np.empty([N])
     for i in range(D):
         for j in range(N):
             temp[j] = np.random.uniform(
-                low=j * d, high=(j + 1) * d, size = 1)[0]
-
+                low=j * d, high=(j + 1) * d, size=1)[0]
         np.random.shuffle(temp)
-
         for j in range(N):
             result[j, i] = temp[j]
-
-    lower_bounds = lb
-    upper_bounds = ub
-    if np.any(lower_bounds > upper_bounds):
+    if np.any(lower_bound > upper_bound):
+        print('Range error')
         return None
-    #   sample * (upper_bound - lower_bound) + lower_bound
-    np.add(np.multiply(result,
-                       (upper_bounds - lower_bounds),
-                       out=result),
-           lower_bounds,
-           out=result)
+    np.add(np.multiply(result, (np.array(upper_bound) - np.array(lower_bound)), out=result), lower_bound, out=result)
     return result
