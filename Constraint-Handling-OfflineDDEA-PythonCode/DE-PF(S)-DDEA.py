3b6b25b9b59e711725527b55369088f3a4dc08cf
Pengfei Huang <pfeihuang@foxmail.com>
2021-04-29 14:41:24 +0800

    Update and rename DE-PF(S)-DDEA to DE-PF(S)-DDEA.py

--- Constraint-Handling-OfflineDDEA-PythonCode/DE-PF(S)-DDEA.py
+++ Constraint-Handling-OfflineDDEA-PythonCode/DE-PF(S)-DDEA.py
@@ -34,23 +34,6 @@ from Latin import latin
 import warnings
 warnings.filterwarnings("ignore")
 
-
-def latin(N, D, lower_bound, upper_bound):
-    d = 1.0 / N
-    result = np.empty([N, D])
-    temp = np.empty([N])
-    for i in range(D):
-        for j in range(N):
-            temp[j] = np.random.uniform(
-                low=j * d, high=(j + 1) * d, size=1)[0]
-        np.random.shuffle(temp)
-        for j in range(N):
-            result[j, i] = temp[j]
-    if np.any(lower_bound > upper_bound):
-        print('Range error')
-        return None
-    np.add(np.multiply(result, (np.array(upper_bound) - np.array(lower_bound)), out=result), lower_bound, out=result)
-    return result
 class DE():
     def __init__(self, max_iter, dimension, ub, lb, pop_size, F, CR):
         self.pop_size = pop_size  # 种群数量
